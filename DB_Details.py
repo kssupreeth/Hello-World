00d572761158289049b70d9c3a78f1f4cd9cd7f2
kssupreeth <supreethrocks@gmail.com>
2020-07-14 14:14:19 +0530

    Create DB_Details.py

---
+++ DB_Details.py
@@ -0,0 +1,7 @@
+RIQ_DATABASE='RIQDEV'
+RIQ_HOSTNAME='prdpcrhriq02.w3-969.ibm.com'
+RIQ_PORT=50010
+RIQ_UID='db2inst1'
+RIQ_PWD='J-u-l-10-Resource'
+RIQ_SSLServerCertificate='C:/Users/SupreethSureshChandr/PycharmProjects/Python/venv/Python_git/riqdb2-selfsigned-v2.arm'
+RIQ_SECURITY='ssl'
